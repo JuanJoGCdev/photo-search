996d8a975556a4c1c67c3878cd855a872f31c453
JuanJoGCdev <juanjosse.giraldo@globant.com>
2024-07-25 22:12:30 -0500

    Add loaderCard

---
+++ src/components/LoaderCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './LoaderCard.scss'
+
+export const LoaderCard = () => {
+  return (
+    <div class="card">
+      <div class="card__skeleton card__description"> </div>
+    </div>
+  );
+};
--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -1,18 +1,61 @@
-import React from "react";
+// src/pages/Collections.js
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Header from "../components/Header";
 import MenuBar from "../components/MenuBar";
 import './Collections.scss';
 import useCollections from "../hooks/useCollections";
+import { LoaderCard } from "../components/LoaderCard";
 
 const Collections = () => {
   const { collectionName } = useParams(); // Extrae el parámetro de la URL
   const collections = useCollections();
 
+  // Estado para manejar el estado de carga global
+  const [loading, setLoading] = useState(true);
+  // Estado para manejar la carga de imágenes individuales
+  const [imageLoading, setImageLoading] = useState({});
+
   // Filtra la colección basada en el nombre obtenido de la URL
   const filteredCollection = collections.find(
     (collection) => collection.name.toLowerCase() === collectionName.toLowerCase()
   );
+
+  useEffect(() => {
+    if (collections.length > 0) {
+      setLoading(false);
+    }
+  }, [collections]);
+
+  // Maneja el estado de carga de cada imagen
+  const handleImageLoad = (id) => {
+    setImageLoading((prevState) => ({
+      ...prevState,
+      [id]: false,
+    }));
+  };
+
+  const handleImageError = (id) => {
+    setImageLoading((prevState) => ({
+      ...prevState,
+      [id]: false,
+    }));
+  };
+
+  // Inicializa el estado de carga para cada imagen
+  useEffect(() => {
+    if (filteredCollection) {
+      const initialLoadingState = {};
+      filteredCollection.contentCollection.forEach((content) => {
+        initialLoadingState[content.id] = true;
+      });
+      setImageLoading(initialLoadingState);
+    }
+  }, [filteredCollection]);
+
+  if (loading) {
+    return <div className="loading">Loading...</div>; // Mensaje de carga global
+  }
 
   if (!filteredCollection) {
     return <div>No collections found for {collectionName}</div>;
@@ -28,15 +71,32 @@ const Collections = () => {
             {filteredCollection.name || "Collection"}
           </h1>
           <div className="collectionsImgContainer">
-            <img src={filteredCollection.cover} alt="Cover" className="collectionsImg" />
+            
+            <img
+              src={filteredCollection.cover}
+              alt="Cover"
+              className="collectionsImg"
+              onLoad={() => handleImageLoad('cover')}
+              onError={() => handleImageError('cover')}
+              style={{ display: imageLoading['cover'] ? 'none' : 'block' }}
+            />
             {filteredCollection.contentCollection.length > 0 ? (
               filteredCollection.contentCollection.map((content) => (
-                <img
-                  className="collectionsImg"
-                  key={content.id}
-                  src={content.urls.full}
-                  alt={content.alt_description || "Collection image"}
-                />
+
+                <div key={content.id} className="imageWrapper">
+                          
+                  {imageLoading[content.id] && (
+                    <LoaderCard/>
+                  )}
+                  <img
+                    className="collectionsImg"
+                    src={content.urls.full}
+                    alt={content.alt_description || "Collection image"}
+                    onLoad={() => handleImageLoad(content.id)} // Marca la imagen como cargada
+                    onError={() => handleImageError(content.id)} // Maneja el error de carga
+                    style={{ display: imageLoading[content.id] ? 'none' : 'block' }} // Oculta la imagen mientras se carga
+                  />
+                </div>
               ))
             ) : (
               <p>No images available for {filteredCollection.name}</p>
